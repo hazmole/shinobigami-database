4d7d123177cff47fc06d3970baf4773d1faa303f
hazmole <buzzfirefox@gmail.com>
2022-01-27 02:20:23 +0800

    add auth-page & style

---
+++ js/authenticator.js
@@ -0,0 +1,44 @@
+$(document).ready(checkAuthentication);
+
+function checkAuthentication(){
+	function isAtAuthPage(){
+		return window.location.href.match(/auth\.html$/)!=null;
+	}
+	function hasSetCypher(){
+		return localStorage.getItem('shinobi-auth-cypher')!=null;
+	}
+
+	//===========================
+	if(isAtAuthPage()){
+		return hasSetCypher()? null: setRandCypher();
+	}
+
+	var status = localStorage.getItem('shinobi-auth-status');
+	if(!status){
+		window.location.replace("./auth.html")
+	}
+}
+
+function auth(input){
+	//TODO: auth 
+
+	localStorage.setItem('shinobi-auth-status', true);
+	window.location.replace("./index.html")
+}
+
+function setRandCypher(){
+	function randInt(max){
+		return Math.floor(Math.random()*max);
+	}
+	//===========================
+	var cypher = [];
+	cypher.push(randInt(6));
+	cypher.push(randInt(6));
+	cypher.push(randInt(6));
+	localStorage.setItem('shinobi-auth-cypher', cypher);
+}
+
+function clearCypherStatus(){
+	localStorage.setItem('shinobi-auth-cypher', null);
+	localStorage.setItem('shinobi-auth-status', false);
+}
